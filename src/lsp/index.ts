21cf98984f8c47f4772d7e0eb611b109f9a6a12f
Carlos Ramirez <carlos@extrasistemas.com>
2020-09-10 15:15:08 -0500

    added example LSP
    added file .gitignore

--- src/lsp/index.ts
+++ src/lsp/index.ts
@@ -1,20 +1,79 @@
-class Rectangle {
-    #width: number;
-    #height: number;
-    constructor() {
-        this.#width = 0;
-        this.#height = 0;
+//  VIOLACION DEL => Principio de sustitución de Liskov
+class BadRectangle {
+    protected _width: number = 0;
+    protected _height: number = 0;
+    constructor(width: number = 0, height: number=0) {
+        this._width = width;
+        this._height = height;
     }
-    set width(width: number): void {
-        this.#width = width;
+    public setWidth($width: number): void {
+        this._width = $width;
     }
 
-    set height(height: number): void {
-        this.#height = height;
+    setHeight($height: number): void {
+        this._height = $height;
     }
 
     public getArea(): number{
-        return this.#width * this.#height;
+        return this._width * this._height;
     }
 }
 
+class BadSquare extends BadRectangle {
+    public setWidth(width: number): void{
+        this._width = width;
+        this._height = width;
+    }
+    public setHeight(height: number): void{
+        this._width = height;
+        this._height = height;
+    }
+}
+
+const instance = new BadSquare(10,14);
+console.log('################ BAD LSP ###############');
+instance.setWidth(20);
+console.log(
+    instance.getArea()
+    );
+    instance.setHeight(10);
+console.log(
+    instance.getArea()
+);
+
+
+// APLICACION CORRECTA DEL Principio de sustitución de Liskov
+class Figure{
+    public width: number = 0;
+    public height: number = 0;
+    public constructor() {
+    }
+    public getArea(): number{
+        return this.width * this.height;
+    }
+}
+
+class Rectangle extends Figure{
+    public constructor(width: number, height: number){
+        super();
+        this.width = width;
+        this.height = height
+    }
+}
+
+class Square extends Rectangle{
+    public constructor (length: number) {
+        super();
+        this.width = length;
+        this.height = length;
+    }
+}
+console.log('################ GOOD LSP ###############');
+const goodLspSquare = new Square(2);
+console.log(
+    goodLspSquare.getArea()
+    );
+const goodLspRectangle = new Rectangle(2,3);
+console.log(
+    goodLspRectangle.getArea()
+);
