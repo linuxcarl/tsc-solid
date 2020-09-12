9bca2c3dc73357478ac843cf0c611121d6dd9ab8
Carlos Ramirez <carlos@extrasistemas.com>
2020-09-12 08:21:33 -0500

    second option bad implements dependency injection

--- src/dip/index.ts
+++ src/dip/index.ts
@@ -1,4 +1,4 @@
-class UseCase {
+/*class UseCase {
   private externalService: any;
   public constructor() {
     this.externalService = new ExternalService();
@@ -12,6 +12,21 @@ class ExternalService {
   public doExternalTask(): string {
     return 'Doing task...';
   }
+}*/
+class UseCase {
+  private externalService: any;
+  public constructor(externalService: ExternalService) {
+    this.externalService = externalService;
+  }
+  public doSomething(): string {
+    return this.externalService.doExternalTask();
+  }
 }
-const useCase = new UseCase();
+
+class ExternalService {
+  public doExternalTask(): string {
+    return 'Doing task...';
+  }
+}
+const useCase = new UseCase(new ExternalService());
 console.log(useCase.doSomething());
