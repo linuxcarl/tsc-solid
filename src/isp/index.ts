dbecfcf4d57de6a941f7e3cf388bb5e128c4c9c6
Carlos Ramirez <carlos@extrasistemas.com>
2020-09-12 01:24:06 -0500

    ·implements class ModelS and Mustang in the file index

--- src/isp/index.ts
+++ src/isp/index.ts
@@ -1,2 +1,9 @@
-import { Car } from './interfaces/car';
-import { Tesla } from './interfaces/tesla';
+import { Mustang } from './class/mustang';
+import { ModelS } from './class/tesla';
+
+const teslaModelS = new ModelS();
+const muestang = new Mustang();
+
+console.log(muestang.startEngine());
+
+console.log(teslaModelS.ludicrousSpeed());
