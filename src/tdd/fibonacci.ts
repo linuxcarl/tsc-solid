436bb3ea5b1d212dfa8515401250efa187eefafe
Carlos Ramirez <carlos@extrasistemas.com>
2020-09-13 14:43:51 -0500

    Refactoring fibonacci

--- src/tdd/fibonacci.ts
+++ src/tdd/fibonacci.ts
@@ -1,3 +1,6 @@
-export function fibonacci(n: number): number {
-  return n;
+export function fibonacci(num: number): number {
+  const partialFibonacci = (n: number): number =>
+    n == 1 ? 1 : fibonacci(n - 1) + fibonacci(n - 2);
+
+  return num == 0 ? num : partialFibonacci(num);
 }
