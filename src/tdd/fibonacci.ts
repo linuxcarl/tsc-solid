f833d4d39e3a2d7e7c5548450455b039c2822cf2
Carlos Ramirez <carlos@extrasistemas.com>
2020-09-13 14:11:49 -0500

    added test with 1 in function fibonacci. error test
    added config to package.json by run test pre-commit

--- src/tdd/fibonacci.ts
+++ src/tdd/fibonacci.ts
@@ -1,3 +1,3 @@
 export function fibonacci(n: number): number {
-  return 0;
+  return n;
 }
--- test/tdd/fibonacci.spec.ts
+++ test/tdd/fibonacci.spec.ts
@@ -4,4 +4,7 @@ describe('Fibonacci should', () => {
   it('return zero if receive zero', () => {
     expect(fibonacci(0)).toBe(0);
   });
+  it('return one if receive one', () => {
+    expect(fibonacci(1)).toBe(1);
+  });
 });
